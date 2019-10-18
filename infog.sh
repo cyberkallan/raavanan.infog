8368f3066b1932f25ff60cf93a8bd6e9efacca06
Cyber kallan <56509491+cyberkallan@users.noreply.github.com>
2019-10-18 12:01:50 +0530

    Update infog.sh

--- infog.sh
+++ infog.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # InfoG v1.0
-# coded by @thelinuxchoice (Don't change, read the lincense!)
-# Github: github.com/thelinuxchoice/infoG
-# Instagram: @thelinuxchoice
+# coded by cyber kallan (Don't change, read the lincense!)
+# Github: https://github.com/cyberkallan/raavanan.infog
+# Instagram: arz_beats
+# YouTube : CYBER KALLAN
 
 banner() {
 printf "\n"
@@ -14,10 +15,10 @@ printf "\e[1;77m       .:: .::  .::  .::   .::  .:: \e[0m\e[1;93m.::         \e[
 printf "\e[1;77m       .:: .::  .::  .::  .::    .::\e[0m\e[1;93m.::   .:::: \e[0m\n"
 printf "\e[1;77m       .:: .::  .::  .::   .::  .::  \e[0m\e[1;93m.::    .:  \e[0m\n"
 printf "\e[1;77m       .::.:::  .::  .::     .::      \e[0m\e[1;93m.:::::    \e[0m\n"
-printf "\e[1;77m     ::::::::::::::::::::::::::::::::::::::::::\e[0m\n"
+printf "\e[1;77m     ::::::::::::::::::::::::::::::::::::::::::r̼̯̤̈ͭ̃ͨ̆a̘̫͈̭͌͛͌̇̇̍a̘̫͈̭͌͛͌̇̇̍v̪̩̜̜̙̜ͨ̽̄a̘̫͈̭͌͛͌̇̇̍n͉̠̙͉̗̺̋̋̔ͧ̊a̘̫͈̭͌͛͌̇̇̍n͉̠̙͉̗̺̋̋̔ͧ̊\e[0m\n"
 printf "\n"
 printf "\e[1;92m     .::.\e[0m\e[1;77m Infomation Gathering Tool - v1.0 \e[1;92m.::.\e[0m\n"
-printf "\e[1;32m  .::.        Coded by @thelinuxchoice        .::.\e[0m\n"
+printf "\e[1;32m  .::.        Coded by Cyber kallan        .::.\e[0m\n"
 printf "\n"
 
 }
